61efd2a38273475bf8aeb3a1c5bbe6869748faa9
Adriano J. Holanda <ajholanda@gmail.com>
2019-07-17 18:40:18 -0300

    h-index calculation done using channels

--- hindex.go
+++ hindex.go
@@ -1,37 +1,42 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
 	"log"
-	"os"
-	"regexp"
 	"strconv"
 	"strings"
 )
 
-const (
-	FileExt  = ".csv"
-	FieldSep = ","
-)
+type HIndex Index
 
-type HIndex struct {
-	Idx      int
-	FileName string
+var hFileAttrs = FileAttrs{
+	FileExt:           ".csv",
+	FieldSep:          ",",
+	LineIgnoredRegExp: `^.{0,3}Article|Timespan|AUTHOR|\"Title\"`,
 }
 
-func (i *HIndex) getValue() int {
-	return i.Idx
+func NewHIndex() *HIndex {
+	return &HIndex{FileAttrs: &hFileAttrs}
 }
 
-func (i *HIndex) getFileName() string {
+func (i HIndex) GetFileName() string {
 	return i.FileName
 }
 
-func count_citations(line string) (c int) {
+func (i HIndex) SetFileName(fileName string) {
+	i.FileName = fileName
+}
+
+func (i HIndex) GetValue() int {
+	return i.Value
+}
+
+// "Title","Authors","Corporate Authors","Editors","Book Editors","Source Title","Publication Date","Publication Year","Volume","Issue","Part Number","Supplement","Special Issue","Beginning Page","Ending Page","Article Number","DOI","Conference Title","Conference Date","Total Citations","Average per Year","1900","1901","1902","1903","1904","1905","1906","1907","1908","1909","1910","1911","1912","1913","1914","1915","1916","1917","1918","1919","1920","1921","1922","1923","1924","1925","1926","1927","1928","1929","1930","1931","1932","1933","1934","1935","1936","1937","1938","1939","1940","1941","1942","1943","1944","1945","1946","1947","1948","1949","1950","1951","1952","1953","1954","1955","1956","1957","1958","1959","1960","1961","1962","1963","1964","1965","1966","1967","1968","1969","1970","1971","1972","1973","1974","1975","1976","1977","1978","1979","1980","1981","1982","1983","1984","1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019"
+func count_citations(line string) int {
 	start_citations := false
 	sum := 0
 
-	fields := strings.Split(line, FieldSep)
+	fields := strings.Split(line, hFileAttrs.FieldSep)
 
 	if len(fields) <= 1 {
 		return 0
@@ -44,7 +49,6 @@ func count_citations(line string) (c int) {
 			start_citations = true
 		}
 		if start_citations {
-
 			field = strings.Replace(field, "\"", "", -1)
 			c, err := strconv.Atoi(field)
 			if err != nil {
@@ -56,53 +60,32 @@ func count_citations(line string) (c int) {
 	return sum
 }
 
-// "Title","Authors","Corporate Authors","Editors","Book Editors","Source Title","Publication Date","Publication Year","Volume","Issue","Part Number","Supplement","Special Issue","Beginning Page","Ending Page","Article Number","DOI","Conference Title","Conference Date","Total Citations","Average per Year","1900","1901","1902","1903","1904","1905","1906","1907","1908","1909","1910","1911","1912","1913","1914","1915","1916","1917","1918","1919","1920","1921","1922","1923","1924","1925","1926","1927","1928","1929","1930","1931","1932","1933","1934","1935","1936","1937","1938","1939","1940","1941","1942","1943","1944","1945","1946","1947","1948","1949","1950","1951","1952","1953","1954","1955","1956","1957","1958","1959","1960","1961","1962","1963","1964","1965","1966","1967","1968","1969","1970","1971","1972","1973","1974","1975","1976","1977","1978","1979","1980","1981","1982","1983","1984","1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019"
-func Calculate(fileName string) (n int, err error) {
-	lineIgnored := regexp.MustCompile(`^.{0,3}Article|Timespan|AUTHOR`)
+func (h HIndex) Calculate(fileName string) (n int, err error) {
+	line := make(chan string)
+	var i int
 
-	file, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	go Read(hFileAttrs, fileName, line)
 
-	reader := bufio.NewReader(file)
-
-	var line string
-	i := 0
-	for {
-		line, _ = reader.ReadString('\n')
-		// Remove any end of line
-		line = strings.TrimRight(line, "\r\n")
-
-		if lineIgnored.FindString(line) != "" {
-			continue
-		}
-
-		// Process the line here.
-		c := count_citations(line)
-
-		// Prevent any well-formed line
-		if c == 0 {
-			continue
-		}
-
-		i = i + 1
-		// Found h-index that is the last i
+	for i = 1; ; i++ {
+		c := count_citations(<-line)
+		fmt.Printf("i=%d > c=%d?\n", i, c)
 		if i > c {
-			i = i - 1
+			i--
 			break
 		}
 	}
 	return i, nil
 }
 
-func ProcessFiles() (hs []HIndex) {
-	regex := "*" + FileExt
+func ProcessHFiles() (is []*HIndex) {
+	//regex := "*" + hFileAttrs.FileExt
+	regex := "olive_k_a-ppf.csv"
 	fileNames := ListFiles(regex)
 	for _, fn := range fileNames {
-		h, _ := Calculate(fn)
-		hs = append(hs, HIndex{h, fn})
+		h := NewHIndex()
+		h.FileName = fn
+		h.Value, _ = h.Calculate(fn)
+		is = append(is, h)
 	}
-	return hs
+	return is
 }
--- index.go
+++ index.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 const (
@@ -12,12 +17,75 @@ const (
 	DataDir = "data/"
 )
 
-// An Index interface propose a method to
+// An Indexer interface propose a method to
 // perform the index calculation that returns
 // an int value corresponding to the index and
 // an error if something goes wrong.
-type Index interface {
+type Indexer interface {
 	Calculate(fileName string) (n int, err error)
+}
+
+type Index struct {
+	Value int // Index value.
+	// File name to parse and calculate the index.
+	FileName string
+	// Common file attributes for the index.
+	FileAttrs *FileAttrs
+}
+
+type FileAttrs struct {
+	FileExt  string // File extension
+	FieldSep string // What character separate fields
+	// regex string composed by rules that may be
+	// applied to ignore lines.
+	LineIgnoredRegExp string
+}
+
+func (i Index) GetFileName() string {
+	return i.FileName
+}
+
+func (i Index) SetFileName(fileName string) {
+	i.FileName = fileName
+}
+
+func (i Index) GetValue() int {
+	return i.Value
+}
+
+// Read a file ignoring some lines.
+// Use chanel to send the line to be processed.
+func Read(fileAttrs FileAttrs, fileName string, line chan string) {
+	rules := fileAttrs.LineIgnoredRegExp
+	lineIgnored := regexp.MustCompile(rules)
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	var ln string
+	for {
+		ln, err = reader.ReadString('\n')
+
+		// Remove any end of line
+		ln = strings.TrimRight(ln, "\r\n")
+		if lineIgnored.FindString(ln) != "" || ln == "" {
+			continue
+		}
+
+		if err != nil {
+			close(line)
+			break
+		}
+
+		// Process the line here.
+		fmt.Println(ln)
+		line <- ln
+	}
 }
 
 // List files from data directory
